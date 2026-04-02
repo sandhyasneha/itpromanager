dde8a8de69047c9f04399295043491e422fb5ad5
ram <chan2006@gmail.com>
2026-04-02 10:15:57 +0530

    fix: exclude app-build-manifest from PWA precache

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  buildExcludes: [/middleware-manifest\.json$/],
+  buildExcludes: [/middleware-manifest\.json$/, /app-build-manifest\.json$/],
   publicExcludes: ['!robots.txt'],
 })
 
@@ -19,3 +19,5 @@ const nextConfig = {
 }
 
 module.exports = withPWA(nextConfig)
+
+
